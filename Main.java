8d8d244efe20e29b5788d92d51c439bbe17272e5
Ragavi <49680219+RagaviVenkatesh@users.noreply.github.com>
2024-07-25 14:45:52 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+package app;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+	public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello JavaFX");
+
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(event -> System.out.println("Hello World!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
